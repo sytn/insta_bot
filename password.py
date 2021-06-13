ee55f8bbefd4d8b50d0a3d9eb137abaa5f96ec20
sytn <berkcanalkan978@gmail.com>
2021-06-14 02:02:49 +0300

    some changes

--- password.py
+++ password.py
@@ -1,7 +1,7 @@
 import string
 import random as rd
 
-def create_pass(length=10, lower=string.ascii_lowercase, upper=string.ascii_uppercase, num=string.digits, symbols=string.punctuation):
+def create_pass(length=20, lower=string.ascii_lowercase, upper=string.ascii_uppercase, num=string.digits, symbols=string.punctuation):
 	xxx = lower + upper + num + symbols
 	temp = rd.sample(xxx, length)
 	password = "".join(temp)
