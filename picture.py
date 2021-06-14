98bcb9642240509921a5e94d959bb6b31b0e8f30
sytn <berkcanalkan978@gmail.com>
2021-06-14 03:12:16 +0300

    fixed

--- picture.py
+++ picture.py
@@ -1,8 +1,27 @@
 import requests
 from identity import create_name
 
-url = 'https://thispersondoesnotexist.com/'
-
+url = 'https://thispersondoesnotexist.com/image'
 filename = f'{create_name()}.png'
-r = requests.get(url, allow_redirects=True)
-open(filename, 'wb').write(r.content)
+response = requests.get(url)
+
+with open(f'C:/Users/PC/Desktop/code/insta_bot/img/{filename}', 'wb') as f:
+	f.write(response.content)
+
+
+
+
+
+
+#file = open(filename, "wb")
+#file.write(response.content)
+#file.close()
+
+# import requests
+# from identity import create_name
+
+# url = 'https://thispersondoesnotexist.com/image'
+
+# filename = f'{create_name()}.png'
+# r = requests.get(url, allow_redirects=True)
+# open(filename, 'wb').write(r.content)
